bdc0ab4df1313a43efa530fdc6fdd37f3794e38f
Ricardo Lopez <00107521@uca.edu.sv>
2024-09-27 11:13:51 -0600

    Add: insert and print functions

--- Programas de consola/C++/Data Structs/Simply linked list/functions.hpp
+++ Programas de consola/C++/Data Structs/Simply linked list/functions.hpp
@@ -14,7 +14,7 @@ class Simple_linked_list {
     public:
         Simple_linked_list();
         Node* create_node(int data);
-        void insert_node_in_list(Node*);
+        void insert_node_in_list(int data);
         void update_node_in_list(int data, int new_data);
         void delete_node_in_list(int data);
         void print_list();
@@ -22,15 +22,45 @@ class Simple_linked_list {
 };
 
 Simple_linked_list::Simple_linked_list() {
-    head->data = NULL;
+    head = new Node();
+
+    head->data = 0;
     head->next = nullptr;
 }
 
 Node* Simple_linked_list::create_node(int data) {
-    Node* new_node = (Node*)malloc(sizeof(Node));
+    Node* new_node = new Node;
 
     new_node->data = data;
     new_node->next = nullptr;
+
+    return new_node;
+}
+
+void Simple_linked_list::insert_node_in_list(int data) {
+    Node* new_node = create_node(data);
+    Node* current_node = head;
+
+    while (current_node->next != nullptr && current_node->next->data < new_node->data) {
+        current_node = current_node->next;  // Avanza al siguiente nodo
+    }
+
+    new_node->next = current_node->next;
+    current_node->next = new_node;
+}
+
+void Simple_linked_list::print_list() {
+    Node* print_node = head->next;
+
+    if (head->next == NULL)
+    {
+        cout << "The list is empty!" << endl; 
+    } else {
+        while(print_node != NULL) {
+            cout << print_node->data << " ";
+            print_node = print_node->next;
+        }
+    }  
 }
 
 #endif
--- Programas de consola/C++/Data Structs/Simply linked list/main.cpp
+++ Programas de consola/C++/Data Structs/Simply linked list/main.cpp
@@ -4,7 +4,15 @@ using namespace std;
 
 #include "functions.hpp"
 
+Simple_linked_list simple_linked_list;
+
 int main () {
+
+    simple_linked_list.insert_node_in_list(1);
+    simple_linked_list.insert_node_in_list(3);
+    simple_linked_list.insert_node_in_list(2);
+
+    simple_linked_list.print_list();
 
     return 0;
 }
