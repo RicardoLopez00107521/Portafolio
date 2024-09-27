7a60cf4c7331d08573bfd62e4792f5eb2ac6ca72
Ricardo Lopez <00107521@uca.edu.sv>
2024-09-27 11:59:21 -0600

    Add: search and update functions

--- Programas de consola/C++/Data Structs/Simply linked list/main.cpp
+++ Programas de consola/C++/Data Structs/Simply linked list/main.cpp
@@ -8,10 +8,15 @@ Simple_linked_list simple_linked_list;
 
 int main () {
 
-    simple_linked_list.insert_node_in_list(1);
+    simple_linked_list.insert_node_in_list(0);
+    simple_linked_list.insert_node_in_list(-1);
+    simple_linked_list.insert_node_in_list(8);
     simple_linked_list.insert_node_in_list(3);
-    simple_linked_list.insert_node_in_list(2);
 
+    simple_linked_list.print_list();
+    cout << "\n";
+
+    simple_linked_list.update_node_in_list(3, 5);
     simple_linked_list.print_list();
 
     return 0;
