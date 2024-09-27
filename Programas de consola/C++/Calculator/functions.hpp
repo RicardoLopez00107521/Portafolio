3623fe290ea0596441c2b2f12d53cf8f1bde915f
Ricardo Lopez <00107521@uca.edu.sv>
2024-09-26 19:29:48 -0600

    Add: First programm

---
+++ Programas de consola/C++/Calculator/functions.hpp
@@ -0,0 +1,6 @@
+#ifndef MENU_HPP
+#define MENU_HPP
+
+//  The calculator functions will be in me (addition, subtraction, multiplication, division)
+
+#endif
---
+++ Programas de consola/C++/Calculator/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+#include "menu.hpp"
+#include "functions.hpp"
+
+int main() {
+    return 0;
+}
