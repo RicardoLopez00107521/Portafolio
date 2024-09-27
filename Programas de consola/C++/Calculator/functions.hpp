8afd1c78900370cc59c2699c5702e2d5f4218082
Ricardo Lopez <00107521@uca.edu.sv>
2024-09-26 23:25:41 -0600

    Add: sum and validate entry function

--- Programas de consola/C++/Calculator/functions.hpp
+++ Programas de consola/C++/Calculator/functions.hpp
@@ -1,6 +1,22 @@
-#ifndef MENU_HPP
-#define MENU_HPP
+#ifndef FUNCTIONS_HPP
+#define FUNCTIONS_HPP
 
-//  The calculator functions will be in me (addition, subtraction, multiplication, division)
+#include <limits>
+
+using namespace std;
+
+int sum_two_numbers(int first_number, int second_number) {
+    return first_number + second_number;
+}
+
+bool validate_entry(int& entry) {
+    if (!(cin >> entry)) {
+        cout << "Invalid entry, try again!.\n";
+        cin.clear(); 
+        cin.ignore(numeric_limits<streamsize>::max(), '\n'); 
+        return false; 
+    }
+    return true;
+}
 
 #endif
--- Programas de consola/C++/Calculator/main.cpp
+++ Programas de consola/C++/Calculator/main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <limits>
+
 
 #include "menu.hpp"
 #include "functions.hpp"
