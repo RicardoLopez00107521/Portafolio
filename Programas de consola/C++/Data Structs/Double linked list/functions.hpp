fd0e4e744605ca6a159abc9e87ec2ca478d4c94d
Ricardo Lopez <00107521@uca.edu.sv>
2024-09-27 12:34:06 -0600

    Add: Constructor function

--- Programas de consola/C++/Data Structs/Double linked list/functions.hpp
+++ Programas de consola/C++/Data Structs/Double linked list/functions.hpp
@@ -20,7 +20,15 @@ class Double_linked_list {
         void update_node_in_list(int data, int new_data);
         void delete_node_in_list(int data);
         void print_list();
-        ~Double_linked_list();
+        //~Double_linked_list();
 };
+
+Double_linked_list::Double_linked_list() {
+    Node* head = new Node;
+
+    head->data = 0;
+    head->next = nullptr;
+    head->previous = nullptr;
+}
 
 #endif
