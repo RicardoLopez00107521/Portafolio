0ce8868ae0f8d8bf71075257374ede0e4ada6a52
Ricardo Lopez <00107521@uca.edu.sv>
2024-09-27 12:31:09 -0600

    Initilialization

---
+++ Programas de consola/C++/Data Structs/Double linked list/functions.hpp
@@ -0,0 +1,26 @@
+#ifndef FUNCTIONS_HPP
+#define FUNCTIONS_HPP
+
+struct Node {
+    int data;
+    Node* next;
+    Node* previous;
+};
+
+class Double_linked_list {
+    private:
+        Node* head;
+        int _size = 0;
+
+    public:
+        Double_linked_list();
+        Node* create_node(int data);
+        void insert_node_in_list(int data);
+        Node* search_node(int data);
+        void update_node_in_list(int data, int new_data);
+        void delete_node_in_list(int data);
+        void print_list();
+        ~Double_linked_list();
+};
+
+#endif
