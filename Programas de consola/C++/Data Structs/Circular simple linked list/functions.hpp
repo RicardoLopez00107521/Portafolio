5bc4426bb5dc3f31bd4b359ec57d20da91cdbfcd
Ricardo Lopez <00107521@uca.edu.sv>
2024-09-27 18:50:10 -0600

    Add: create_node function

--- Programas de consola/C++/Data Structs/Circular simple linked list/functions.hpp
+++ Programas de consola/C++/Data Structs/Circular simple linked list/functions.hpp
@@ -13,12 +13,12 @@ class Circular_simple_linked_list {
     
     public:
         Circular_simple_linked_list();
-        Node* create_node(int data);
-        void insert_node_in_list(int data);
-        Node* search_node(int data);
-        Node* search_previous_node(int data);
-        void update_node_in_list(int data, int new_data);
-        void delete_node_in_list(int data);
+        Node* create_node(int);
+        void insert_node_in_list(int);
+        Node* search_node(int);
+        Node* search_previous_node(int);
+        void update_node_in_list(int, int);
+        void delete_node_in_list(int);
         void print_list();
         //~Circular_simple_linked_list();
 };
@@ -28,6 +28,15 @@ Circular_simple_linked_list::Circular_simple_linked_list() {
 
     head->data = 0;
     head->next = nullptr;
+}
+
+Node* Circular_simple_linked_list::create_node(int data) {
+    Node* new_node = new Node();
+
+    new_node->data = data;
+    new_node->next = nullptr;
+
+    return new_node;
 }
 
 #endif
