73a4469006b35a1a048bef7c0cdc05bfe97c4030
Ricardo Lopez <00107521@uca.edu.sv>
2024-09-29 08:56:01 -0600

    Add: insert and print functions

--- Programas de consola/C++/Data Structs/Circular double linked list/functions.hpp
+++ Programas de consola/C++/Data Structs/Circular double linked list/functions.hpp
@@ -20,7 +20,6 @@ class Circular_double_linked_list
         void update_node_in_list(int, int);
         void delete_node_in_list(int);
         void print_list_left_right();
-        Node* obtain_last_node();
         void print_list_right_left();
         //~Circular_double_linked_list();
 };
@@ -39,6 +38,57 @@ Node* Circular_double_linked_list::create_node(int data) {
     new_node->data = data;
     new_node->next = nullptr;
     new_node->previous = nullptr;
+}
+
+void Circular_double_linked_list::insert_node_in_list(int data) {
+    Node* node_to_insert = create_node(data);
+    Node* current_node = head;
+
+    while (current_node->next != head && current_node->next->data < node_to_insert->data) {
+        current_node = current_node->next;
+    }
+
+    if (current_node->next == head) {
+        node_to_insert->next = current_node->next;
+        node_to_insert->previous = current_node;
+
+        current_node->next = node_to_insert;
+        head->previous = node_to_insert;
+    } else {
+        node_to_insert->next = current_node->next;
+        node_to_insert->previous = current_node;
+
+        current_node->next->previous = node_to_insert;
+        current_node->next = node_to_insert;
+    }    
+}
+
+void Circular_double_linked_list::print_list_left_right() {
+    Node* print_node = head->next;
+
+    if (head->next == head) {
+        cout << "The list is empty!" << endl;
+    } else {
+        while (print_node != head) {
+            cout << print_node->data << " ";
+            print_node = print_node->next;
+        }
+        cout << print_node->next->data << endl;
+    }
+}
+
+void Circular_double_linked_list::print_list_right_left() {
+    Node* print_node = head->previous;
+
+    if (head->previous == head) {
+        cout << "The list is empty!" << endl;
+    } else {
+        while (print_node != head) {
+            cout << print_node->data << " ";
+            print_node = print_node->previous;
+        }
+        cout << print_node->previous->data << endl;
+    }
 }
 
 #endif
--- Programas de consola/C++/Data Structs/Circular double linked list/main.cpp
+++ Programas de consola/C++/Data Structs/Circular double linked list/main.cpp
@@ -4,7 +4,18 @@ using namespace std;
 
 #include "functions.hpp"
 
+Circular_double_linked_list circular_double_linked_list;
+
 int main () {
+
+    circular_double_linked_list.insert_node_in_list(2);
+    circular_double_linked_list.insert_node_in_list(1);
+    circular_double_linked_list.insert_node_in_list(3);
+    circular_double_linked_list.insert_node_in_list(4);
+    circular_double_linked_list.insert_node_in_list(5);
+
+    circular_double_linked_list.print_list_left_right();
+    circular_double_linked_list.print_list_right_left();
 
     return 0; 
 }
