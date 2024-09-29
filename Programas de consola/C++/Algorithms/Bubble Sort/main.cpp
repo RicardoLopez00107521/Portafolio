b5cfcad6c2e45a39293b11892029f7446a9e4cdc
Ricardo Lopez <00107521@uca.edu.sv>
2024-09-29 11:58:52 -0600

    bubble_sort function

--- Programas de consola/C++/Algorithms/Bubble Sort/main.cpp
+++ Programas de consola/C++/Algorithms/Bubble Sort/main.cpp
@@ -2,19 +2,21 @@
 
 using namespace std;
 
-int array[20] = {34, 12, 7, 23, 45, 9, 78, 56, 3, 19, 88, 14, 67, 2, 41, 5, 90, 32, 11, 54};
+int arr[20] = {34, 12, 7, 23, 45, 9, 78, 56, 3, 19, 88, 14, 67, 2, 41, 5, 90, 32, 11, 54};
 
 void swap(int&, int&);
 void print_array();
-void bubble_sort(int&);
+void bubble_sort();
 
 int main() {
 
     cout << "Original array" << endl;
     print_array();
 
-    /*cout << endl << "Sorted array" << endl;
-    print_array();*/
+    bubble_sort();
+
+    cout << endl << "Sorted array" << endl;
+    print_array();
 
     return 0;
 }
@@ -27,7 +29,17 @@ void swap(int& a, int& b) {
 
 void print_array() {
     for (int i = 0; i < 20; ++i) {
-        cout << array [i] << " ";
+        cout << arr [i] << " ";
     }
     cout << endl;
+}
+
+void bubble_sort() {
+    for (int j = 0; j < 20 - 1; j++) {
+        for (int i = 0; i < 20 - j - 1; i++) {
+            if (arr[i] > arr[i+1]) {
+                swap(arr[i], arr[i + 1]);
+            }  
+        }
+    }
 }
