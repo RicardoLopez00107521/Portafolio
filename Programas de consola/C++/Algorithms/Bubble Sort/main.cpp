28d9b4f28f6df6dd74731836ec075214b3046bb5
Ricardo Lopez <00107521@uca.edu.sv>
2024-09-29 11:24:57 -0600

    Add: swap function

--- Programas de consola/C++/Algorithms/Bubble Sort/main.cpp
+++ Programas de consola/C++/Algorithms/Bubble Sort/main.cpp
@@ -2,7 +2,18 @@
 
 using namespace std;
 
+int array[20] = {34, 12, 7, 23, 45, 9, 78, 56, 3, 19, 88, 14, 67, 2, 41, 5, 90, 32, 11, 54};
+
+void swap(int&, int&);
+void bubble_sort(int&);
+
 int main() {
 
     return 0;
+}
+
+void swap(int& a, int& b) {
+    int temp = a;
+    a = b;
+    b = temp;
 }
