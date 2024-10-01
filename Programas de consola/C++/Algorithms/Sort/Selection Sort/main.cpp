a41eab8b141aba2bab3266c52b022d85a03cbffd
Ricardo Lopez <00107521@uca.edu.sv>
2024-09-30 20:03:46 -0600

    Add: selection_sort function

--- Programas de consola/C++/Algorithms/Sort/Selection Sort/main.cpp
+++ Programas de consola/C++/Algorithms/Sort/Selection Sort/main.cpp
@@ -5,13 +5,15 @@ using namespace std;
 int arr[20] = {34, 12, 7, 23, 45, 9, 78, 56, 3, 19, 88, 14, 67, 2, 41, 5, 90, 32, 11, 54};
 
 void swap(int&, int&);
-void insertion_sort();
+void selection_sort();
 void print_array();
 
 int main() {
 
     cout << "Original array" << endl;
     print_array();
+
+    selection_sort();
 
     cout << "Sorted array" << endl;
     print_array();
@@ -30,4 +32,16 @@ void print_array() {
         cout << arr[i] << " ";
     }
     cout << endl;
+}
+
+void selection_sort() {
+    for (int j = 0; j < 20 - 1 ; ++j) {
+        int min_index = j;
+
+        for (int i = j + 1; i < 20; ++i) {
+            if (arr[i] < arr[min_index]) 
+                min_index = i;
+        }
+        swap(arr[j], arr[min_index]);
+    }
 }
