8384fb2045faf81a1dc5d32ce55a8e4e2e6bda6a
Ricardo Lopez <00107521@uca.edu.sv>
2024-09-30 18:15:49 -0600

    Add : Print array function

--- Programas de consola/C++/Algorithms/Sort/Insertion sort/main.cpp
+++ Programas de consola/C++/Algorithms/Sort/Insertion sort/main.cpp
@@ -2,11 +2,16 @@
 
 using namespace std;
 
+int arr[20] = {34, 12, 7, 23, 45, 9, 78, 56, 3, 19, 88, 14, 67, 2, 41, 5, 90, 32, 11, 54};
+
 void swap(int&, int&);
 void insertion_sort();
-void print();
+void print_array();
 
 int main() {
+
+    cout << "Original array" << endl;
+    print_array();
 
     return 0;
 }
@@ -15,4 +20,11 @@ void swap(int& a, int& b) {
     int temp = a;
     a = b;
     b = temp; 
+}
+
+void print_array() {
+    for (int i = 0; i < 20; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
 }
