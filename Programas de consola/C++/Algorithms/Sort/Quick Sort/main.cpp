8a24d3fce9a698d6d9245c347420a52eeed3f20f
Ricardo Lopez <00107521@uca.edu.sv>
2024-09-30 21:35:50 -0600

    Add: partition function

--- Programas de consola/C++/Algorithms/Sort/Quick Sort/main.cpp
+++ Programas de consola/C++/Algorithms/Sort/Quick Sort/main.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-void partition(vector<int>&, int, int);
+int partition(vector<int>&, int, int);
 void quick_sort(vector<int>&, int, int);
 void print_array(vector<int>&);
 
@@ -11,4 +11,19 @@ int main() {
     vector<int> arr = {4, 9, 7, 3, 5};
 
     return 0;
+}
+
+int partition(vector<int>& arr, int low, int high) {
+    int pivot = arr[high];
+    int i = low - 1;
+
+    for (int j = low; i < high; ++j) {
+        if (arr[j] <= pivot) {
+            ++i;
+            swap(arr[i], arr[j]);
+        }
+    }
+    swap(arr[i + 1], arr[high]);
+    
+    return i + 1;
 }
