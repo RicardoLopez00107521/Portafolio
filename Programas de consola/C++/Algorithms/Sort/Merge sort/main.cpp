259c82c69c87f840c4c7bdcc3be3b6f440d3f664
Ricardo Lopez <00107521@uca.edu.sv>
2024-09-30 20:41:25 -0600

    Add: merge_sort function

--- Programas de consola/C++/Algorithms/Sort/Merge sort/main.cpp
+++ Programas de consola/C++/Algorithms/Sort/Merge sort/main.cpp
@@ -2,19 +2,28 @@
 
 using namespace std; 
 
-int arr[20] = {34, 12, 7, 23, 45, 9, 78, 56, 3, 19, 88, 14, 67, 2, 41, 5, 90, 32, 11, 54};
-
-void merge_sort();
+void merge_sort(int&, int l, int mid, int r);
 void merge();
 void print_array(); 
 
 int main() {
+    int arr[20] = {34, 12, 7, 23, 45, 9, 78, 56, 3, 19, 88, 14, 67, 2, 41, 5, 90, 32, 11, 54};
 
     cout << "Original array" << endl;
     print_array();
 
     cout << "Sorted array" << endl;
     print_array();
-    
+
     return 0;
+}
+
+void merge_sort(int& arr, int l, int mid, int r) {
+    if (l >= r) return;
+
+    int mid = (l + r) / 2;
+
+    merge_sort(arr, l, mid);
+    merge_sort(arr, mid + 1, r);
+    // Call merge
 }
