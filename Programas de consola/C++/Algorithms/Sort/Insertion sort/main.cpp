88ccebd4c72ea1a42828ba1653c7ec0c6d53443b
Ricardo Lopez <00107521@uca.edu.sv>
2024-09-30 18:26:03 -0600

    Delete: swap function, not is needed

--- Programas de consola/C++/Algorithms/Sort/Insertion sort/main.cpp
+++ Programas de consola/C++/Algorithms/Sort/Insertion sort/main.cpp
@@ -4,7 +4,6 @@ using namespace std;
 
 int arr[20] = {34, 12, 7, 23, 45, 9, 78, 56, 3, 19, 88, 14, 67, 2, 41, 5, 90, 32, 11, 54};
 
-void swap(int&, int&);
 void insertion_sort();
 void print_array();
 
@@ -13,13 +12,11 @@ int main() {
     cout << "Original array" << endl;
     print_array();
 
-    return 0;
-}
+    insertion_sort();
 
-void swap(int& a, int& b) {
-    int temp = a;
-    a = b;
-    b = temp; 
+    cout << "Sorted array" << endl;
+    print_array();
+    return 0;
 }
 
 void print_array() {
