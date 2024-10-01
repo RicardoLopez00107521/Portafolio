7a1b7134e8b371ee095ad03103381427530e8784
Ricardo Lopez <00107521@uca.edu.sv>
2024-09-30 21:17:16 -0600

    Add: print and merge functions, and the elements of the array

--- Programas de consola/C++/Algorithms/Sort/Merge sort/main.cpp
+++ Programas de consola/C++/Algorithms/Sort/Merge sort/main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 void merge_sort(vector<int>&, int l, int r);
 void merge(vector<int>&, int l, int mid, int r);
-void print_array(); 
+void print_array(vector<int>& arr); 
 
 int main() {
     vector<int> arr;
@@ -19,10 +19,12 @@ int main() {
     arr.push_back(8);
 
     cout << "Original array" << endl;
-    print_array();
+    print_array(arr);
+
+    merge_sort(arr, 0, arr.size()-1);
 
     cout << "Sorted array" << endl;
-    print_array();
+    print_array(arr);
 
     return 0;
 }
@@ -34,5 +36,51 @@ void merge_sort(vector<int>& arr, int l, int r) {
 
     merge_sort(arr, l, mid);
     merge_sort(arr, mid + 1, r);
-    // Call merge
+    merge(arr, l, mid, r);
+}
+
+void merge(vector<int>& arr, int l, int mid, int r) {
+    int n1 = mid - l + 1;
+    int n2 = r - mid;
+
+    vector<int> left(n1), right(n2);
+
+    for (int i = 0; i < n1; ++i) 
+        left[i] = arr[l + i];
+
+    for (int j = 0; j < n2; j++){
+        right[j] = arr[(mid+1) + j];
+    }
+
+    int i = 0, j = 0, k = l;
+
+    while (i < n1 && j < n2) {
+        if (left[i] <= right[j]) {
+            arr[k] = left[i];
+            ++i;
+        } else {
+            arr[k] = right[j];
+            ++j;
+        }
+        ++k;
+    }
+
+    while (i < n1) {
+        arr[k] = left[i];
+        ++i; 
+        ++k;
+    }
+    
+    while (j < n2) {
+        arr[k] = right[j];
+        ++j; 
+        ++k;
+    }
+}
+
+void print_array(vector<int>& arr) {
+    for (int i = 0; i < arr.size(); i++) 
+        cout << arr[i] << " ";
+    
+    cout << endl;
 }
