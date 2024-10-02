cdef5d8fb7c8eecc32bf1dfe06813e3b016e6575
Ricardo Lopez <00107521@uca.edu.sv>
2024-10-02 15:08:13 -0600

    Fix get_predecessor function and new input

--- Programas de consola/C++/Algorithms/Search/BST/main.cpp
+++ Programas de consola/C++/Algorithms/Search/BST/main.cpp
@@ -8,23 +8,31 @@ BST bst;
 
 int main() {
 
-    bst.insert_element(100);
-    bst.insert_element(20);
-    bst.insert_element(10);
+    bst.insert_element(37);
+    bst.insert_element(15);
+    bst.insert_element(22);
+    bst.insert_element(8);
+    bst.insert_element(5);
+    bst.insert_element(12);
     bst.insert_element(30);
-    bst.insert_element(500);
-    bst.print();
-
-    cout << endl;
-
-    bst.insert_element(2);
-    bst.print();
-
-    cout << endl;
-
-    bst.search_element(0);
-    cout << endl;
+    bst.insert_element(18);
+    bst.insert_element(25);
+    bst.insert_element(3);
+    bst.insert_element(10);
+    bst.insert_element(20);
+    bst.insert_element(40);
+    bst.insert_element(45);
+    bst.insert_element(50);
+    bst.insert_element(42);
+    bst.insert_element(44);
+    bst.insert_element(48);
+    bst.insert_element(47);
+    bst.insert_element(52);
     
+
+    
+
+    bst.algo();
 
     return 0;
 }
