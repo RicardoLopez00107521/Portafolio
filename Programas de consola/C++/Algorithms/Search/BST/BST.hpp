3ae99057aa065a0a00852784f5a149494694bcde
Ricardo Lopez <00107521@uca.edu.sv>
2024-10-02 11:04:34 -0600

    Add: constructor, insert_element and create node functions

--- Programas de consola/C++/Algorithms/Search/BST/BST.hpp
+++ Programas de consola/C++/Algorithms/Search/BST/BST.hpp
@@ -10,13 +10,41 @@ struct Node {
 class BST {
     private:
         Node* root;
+        Node* create_node(int);
 
     public:
+        BST();
         void insert_element(int);
         Node* search_element(int);
         Node* get_succesor(int);
         void delete_element(int);
         void print_in_order();
 };
+
+BST::BST() {
+    root = new Node();
+
+    root->data = 0;
+    root->left = nullptr;
+    root->riight = nullptr;
+}
+
+Node* BST::create_node(int data) {
+    Node* new_node = new Node();
+
+    new_node->data = data;
+    new_node->left = nullptr;
+    new_node->riight = nullptr;
+}
+
+void BST::insert_element(int data) {
+    if (root->left == nullptr && root->riight == nullptr) {
+        root->data = data;
+    }
+}
+
+void BST::print_in_order() {
+    cout << root->data;
+}
 
 #endif
--- Programas de consola/C++/Algorithms/Search/BST/main.cpp
+++ Programas de consola/C++/Algorithms/Search/BST/main.cpp
@@ -4,7 +4,13 @@ using namespace std;
 
 #include "BST.hpp"
 
+BST bst;
+
 int main() {
 
+    bst.insert_element(5);
+
+    bst.print_in_order();
+    
     return 0;
 }
